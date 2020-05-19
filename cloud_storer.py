1ca3c30e91715c6a6c3a67afc03af209ea378b49
internet-corey <42281648+internet-corey@users.noreply.github.com>
2020-05-19 09:37:16 -0700

    removed typo f-string marker in the print statement

--- cloud_storer.py
+++ cloud_storer.py
@@ -6,4 +6,4 @@ def upload_blob(bucket_name, source_file, destination_name):
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_name)
     blob.upload_from_filename(source_file)
-    print(f'file uploaded')
+    print('file uploaded')
